421847dd712f914068d671cd4ec6345124fe26a4
raphacosta27 <rapha2706@hotmail.com>
2016-05-13 06:57:11 -0300

    Arquivo das funçoes que salvam as informações do calendario criado

--- Calendario.py
+++ Calendario.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui, QtGui
+from PyQt4 import QtCore, QtGui
 import sys
 
 class Ui_Calendario(QtGui.QMainWindow):
